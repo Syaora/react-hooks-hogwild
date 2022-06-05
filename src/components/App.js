a53eb5cf1a49c65f7e38e920d9188e92f346aa9e
Elizabeth Nguyen <saper209@yahoo.com>
2022-06-05 14:46:42 -0700

    Refactor and added Controlled Components

--- src/components/App.js
+++ src/components/App.js
@@ -6,24 +6,22 @@ import Options from "./Options"
 import hogs from "../porkers_data";
 
 function App() {
-	const [hogsList, setHogsList] = useState(hogs)
-	const [showAll, setShowAll] = useState(false)
+	const [showGreased, setShowGreased] = useState(false)
 	const [sortBy, setSortBy] = useState("name")
 
-	function filterGreasedHogs(value){
-		const newList = hogs.filter((hog) => hog.greased || !value)
-		setHogsList(hogsList => newList)
-		setShowAll((showAll) => !showAll)
-	}
-
-	function onSortBy(event){
-		const newList = hogs.sort((hog1, hog2) => {
-			if (event.target.value === "name"){
+	const displayHogs = hogs.filter((hog) => showGreased ? hog.greased : true)
+		.sort((hog1, hog2) => {
+			if (sortBy === "name"){
 				return hog1.name.localeCompare(hog2.name)
 			} else return hog1.weight - hog2.weight
 		})
-		setHogsList(hogsList => newList)
-		setSortBy((sortBy) => event.target.value)
+
+	function onShowGreased(){
+		setShowGreased(showGreased => !showGreased)
+	}
+
+	function onSortBy(event){
+		setSortBy(sortBy => event.target.value)
 	}
 
 	return (
@@ -32,10 +30,10 @@ function App() {
 				<Nav />
 			</div>
 			<div className="ui sixteen wide column centered">
-				<Options onGreasedHogs={filterGreasedHogs} onSortBy={onSortBy} />
+				<Options showGreased={showGreased} onShowGreased={onShowGreased} sortBy={sortBy} onSortBy={onSortBy} />
 			</div>
 			<div className="ui sixteen wide column centered">
-				<HogList hogs={hogsList} />
+				<HogList hogs={displayHogs} />
 			</div>
 		</div>
 	);
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,13 +1,13 @@
-function Options({ onGreasedHogs, onSortBy }) {
+function Options(props) {
   return (
     <div className="ui five column centered grid">
       <div className="column">
         <div className="ui checkbox">
-          <input type="checkbox" name="greased" onChange={(event) => onGreasedHogs(event.target.checked)} />
+          <input type="checkbox" name="greased" checked={props.showGreased} onChange={props.onShowGreased} />
           <label>Show Only Greased Hogs</label>
         </div>
       </div>
-      <select className="ui dropdown" onChange={onSortBy}>
+      <select className="ui dropdown" value={props.sortBy} onChange={props.onSortBy}>
         <option value="name">Name</option>
         <option value="weight">Weight</option>
       </select>
